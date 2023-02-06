a633201a39ee6c7df5a147f9ecaa798b3c8bfd33
rtimoney <103318517+rtimoney@users.noreply.github.com>
2023-02-06 19:13:22 +0000

    Update HelloWorld.java

--- src/first/HelloWorld.java
+++ src/first/HelloWorld.java
@@ -15,16 +15,16 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		// Hello world message 
+		// display the Hello world message 
 System.out.println("Hello World! I am alive.\n");
 System.out.println("I can write on two lines!");
 
-//skip a few lines
+//skip a number of lines by printing newline chars
 System.out.println("\n");
 System.out.println("\n");
 
 
-// Happy Christmas Message 
+// display a Happy Christmas Message 
 System.out.println("\t       Happy Christmas");
 System.out.println("\t \t     *");
 System.out.println("\t \t    ***");
@@ -35,7 +35,7 @@ System.out.println("\t \t  *******");
 System.out.println("\n");
 System.out.println("\n");
 
-// Macbeth Excerpt 
+// Macbeth Excerpt to demonstrate spacing in the console
 System.out.println("First Witch   \t\t When shall we three meet again?");
 System.out.println("              \t\t In thunder, lightening or in rain?\n");
 
@@ -44,26 +44,6 @@ System.out.println("              \t\t When the battle's lost and won.\n");
 
 System.out.println("Third Witch   \t\t That will be ere the set of sun.");
 
+	} // end of main method
 
-
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	}
-
-}
+} end of class
