6520628a21b150d4891f704dfb4b045328340e54
rtimoney <103318517+rtimoney@users.noreply.github.com>
2023-02-07 19:48:22 +0000

    Update DecisionExample.java

--- src/first/DecisionExample.java
+++ src/first/DecisionExample.java
@@ -12,12 +12,12 @@ public class DecisionExample {
 
 	/**
 	 * @param args
-
 	 *
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		// declaring variables
 		int num1, num2;
 		
 		num1 = 1;
@@ -25,19 +25,16 @@ public class DecisionExample {
 		
 		// if statement - if num1 is less than or equal to num2 then everything within {} will happen 
 		if(num1<=num2) {
+			
+			// print a message to the console 
 			System.out.println("This is correct");
 			
 			int total = 3 * 12 * 18;
 
+			// print total to the console
 			System.out.println(total); 
 			
-			
-			
-			
-		}
-	}
-
-
-
-}
+		}// end of if statement
+	} // end of main method
+} // end of class
 
