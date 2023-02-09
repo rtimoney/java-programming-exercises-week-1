bf0e43fdadbb3abfcc3f54130ef18c5231eaa1df
rtimoney <103318517+rtimoney@users.noreply.github.com>
2023-02-09 19:52:59 +0000

    Update StringNamesJava.java

--- src/first/StringNamesJava.java
+++ src/first/StringNamesJava.java
@@ -14,24 +14,20 @@ public class StringNamesJava {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		// Declare vars
 		String yourName;
+	
+		// initialize 
 		yourName = "Ronan \t Timoney";
 
 		System.out.println(yourName);
 		System.out.println("The input string is:\t"+yourName);
-System.out.println("The number of characters in the string is : "+yourName.length());
+		System.out.println("The number of characters in the string is : "+yourName.length());
 
-System.out.println("The first character in the string is : "+yourName.charAt(0));
-System.out.println("The first character in the string is : "+yourName.charAt(1)+yourName.charAt(2));
-System.out.println("The first character in the string is : "+yourName.toUpperCase());
-		
-		
-		
-		
-		
-		
-		
-	}
-
-}
+		System.out.println("The first character in the string is : "+yourName.charAt(0));
+		System.out.println("The first character in the string is : "+yourName.charAt(1)+yourName.charAt(2));
+		System.out.println("The first character in the string is : "+yourName.toUpperCase());
+	
+	} // end of main method 
+} // end of class
