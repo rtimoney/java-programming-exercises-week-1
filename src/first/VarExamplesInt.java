f1de99f833750adf4fc9cee7c67cfc837e4c75ef
rtimoney <103318517+rtimoney@users.noreply.github.com>
2023-02-09 19:54:20 +0000

    Update VarExamplesInt.java

--- src/first/VarExamplesInt.java
+++ src/first/VarExamplesInt.java
@@ -14,10 +14,13 @@ public class VarExamplesInt {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-int myInt;
-myInt = 2021;
-System.out.println("MyInt value is " +myInt);
-myInt = 2022;
-System.out.printf("MyInt value is %d" ,myInt); 
+		
+			int myInt;
+			myInt = 2021;
+			System.out.println("MyInt value is " +myInt);
+		
+			myInt = 2022;
+			System.out.printf("MyInt value is %d" ,myInt); 
+		
 	} // end of main method
 } // end of class
